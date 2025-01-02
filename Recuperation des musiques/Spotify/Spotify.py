4294424553e179546b94054fc97e6359ccaa2c5f
Elliot P <elliotpicavet@me.com>
2025-01-02 17:38:59 +0100

    Récuperation des ID de façon sécurisés.

--- Recuperation des musiques/Spotify/Spotify.py
+++ Recuperation des musiques/Spotify/Spotify.py
@@ -1,12 +1,20 @@
 # Import des librairies
-
-import spotipy
+import sys
+import os
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+print(os.listdir())
+import config
+
 
 # Remplacez par vos identifiants d'application
-CLIENT_ID = "votre_client_id"
-CLIENT_SECRET = "votre_client_secret"
-REDIRECT_URI = "http://localhost:8080"
+SECRET_CLIENT_TOKEN = config.SPOTIFY_SECRET_CLIENT_TOKEN
+CLIENT_ID = config.SPOTIFY_CLIENT_ID
+REDIRECT_URI = config.REDIRECT_URI
+
+
 
 # Configuration OAuth
 scope = "user-read-private"  # Permet de lire les informations utilisateur
